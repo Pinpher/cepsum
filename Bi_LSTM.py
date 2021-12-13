816867bfced6255bab626dd80bfed1d31d4cb74c
Mardeleines <850153472@qq.com>
2021-12-13 22:52:10 +0800

    BiLSTM encoder

---
+++ Bi_LSTM.py
@@ -0,0 +1,47 @@
+import torch
+import logging
+from torch import nn
+import torch.nn.functional as F
+
+class Bi_LSTM(nn.Module):
+    def __init__(self, 
+            input_size,             # embed_dim
+            hidden_size,            # h_dim
+            batch_size):           
+        super().__init__()
+        # fw & bw use different cells
+        self.batch_size = batch_size
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.lstmCell_fw = nn.LSTMCell(input_size=input_size, hidden_size=hidden_size, bias=False)
+        self.lstmCell_bw = nn.LSTMCell(input_size=input_size, hidden_size=hidden_size, bias=False)
+        return
+    
+    def forward(self, x):
+        # x : (bach_size, sentence_length, embed_dim)
+        sentence_length, embed_dim = x[0].shape
+        # forward
+        h_fw, c_fw = self.init()
+        h_fw_each_step = []
+        for i in range(sentence_length):
+            x_i = x[:, i]                                     #(batch_size, 1, embed_dim)
+            h_fw, c_fw = self.lstmCell_fw(x_i, (h_fw, c_fw))  #(batch_size, embed_dim)
+            h_fw_each_step.append(h_fw)
+        # backword
+        h_bw, c_bw = self.init()
+        h_bw_each_step = []
+        for i in range(sentence_length):
+            x_i = x[:, sentence_length - 1 - i]               #(batch_size, 1, embed_dim)
+            h_bw, c_bw = self.lstmCell_bw(x_i, (h_bw, c_bw))  #(batch_size, embed_dim)
+            h_bw_each_step.append(h_bw)
+        # add & output
+        h_bw_each_step.reverse()
+        h_fw_each_step = torch.stack(h_fw_each_step, dim=0)   #(sentence_length, batch_size, embed_dim)
+        h_bw_each_step = torch.stack(h_bw_each_step, dim=0)   #(sentence_length, batch_size, embed_dim)
+        return h_fw_each_step + h_bw_each_step
+    
+    def init(self):
+        # (batch_size, hidden_dim)
+        h0 = torch.zeros(self.batch_size, self.hidden_size)
+        c0 = torch.zeros(self.batch_size, self.hidden_size)
+        return h0, c0
--- train.py
+++ train.py
@@ -1,8 +1,48 @@
+from torch.nn.functional import embedding
 from torch.utils.data import DataLoader
 from mydataset import *
+from Bi_LSTM import *
 
 data = MyDataset("data/cut_valid.txt")
 dataloader = DataLoader(data, batch_size=32, collate_fn=collate_fn)
-for i_batch, batch_data in enumerate(dataloader):
-    print(batch_data)
-    break
+
+embed_dim = 300
+batch_size = 32
+
+# padding for input sectence
+def padding(x):
+    max_len = 0
+    for sentence in x:
+        max_len = max(len(sentence), max_len)
+    for i, sentence in enumerate(x):
+        pad = torch.zeros([max_len - len(sentence), embed_dim])
+        x[i] = torch.cat((sentence, pad), dim=0)
+    return torch.stack(x)
+
+def main():
+    for i_batch, batch_data in enumerate(dataloader):
+        # batch_data: 4-tuple (key_batch, value_batch, src_batch, tgt_batch)
+        # key_batch & value_batch: (batch_size, 
+        #                           attribute list length,      # unfixed 
+        #                           word num of key / value,    # unfixed 
+        #                           embedd_dim)
+        # src_batch & tgt_batch: (batch_size, 
+        #                         word num of src / tgt,        # unfixed 
+        #                         embedd_dim)
+
+        #reshape & padding
+        key_batch = [torch.stack([j for i in batch_data[0][l] for j in i]).squeeze(1) for l in range(batch_size)]
+        value_batch = [torch.stack([j for i in batch_data[1][l] for j in i]).squeeze(1) for l in range(batch_size)]
+        src_batch = [i.squeeze(1) for i in batch_data[2]]
+        tgt_batch = [i.squeeze(1) for i in batch_data[3]]
+
+        model1 = Bi_LSTM(input_size=embed_dim, hidden_size=128, batch_size=batch_size)
+        key_h = model1(padding(key_batch))
+
+        model2 = Bi_LSTM(input_size=embed_dim, hidden_size=128, batch_size=batch_size)
+        tgt_h = model1(padding(tgt_batch))
+
+        break
+
+if __name__ == "__main__":
+    main()
