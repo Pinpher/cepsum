5d2462bd3fec18061aaa8833a7a61f04a2f3d090
Mardeleines <850153472@qq.com>
2021-12-17 14:22:24 +0800

    fix pronunciation

--- mymodel.py
+++ mymodel.py
@@ -77,6 +77,6 @@ class Mymodel(nn.Module):
             indices = t_indices[:,i]
             mask = s_mask[:,i].squeeze(-1).byte()                        
             indices = torch.masked_select(indices, mask)                
-            loss.append(torch.sum(-torch.log([p_g[i][indice] for indice in indices])) / len(indices))
+            loss.append(torch.mean(-torch.log([p_g[i][index] for index in indices])))
 
         return loss.mean()
