b82c98f8159a591d655141b6f794070f71e947fe
Mardeleines <850153472@qq.com>
2021-12-17 12:29:52 +0800

    eval loss

--- train.py
+++ train.py
@@ -14,14 +14,16 @@ embed_dim = 300
 hidden_size = 128
 candidate_size = 100000
 learning_rate = 1e-3
-epoch_num = 5
+epoch_num = 20
 model_save_path = "./model"
 name = "test"
 
 #embedding = MyEmbedding()
 #vocabulary = Myvocabulary()
-data = MyDataset("data/cut_valid.txt")
-dataloader = DataLoader(data, batch_size=batch_size, collate_fn=collate_fn)
+train_data = MyDataset("data/cut_train.txt")
+valid_data = MyDataset("data/cut_valid.txt")
+train_dataloader = DataLoader(train_data, batch_size=batch_size, collate_fn=collate_fn)
+valid_dataloader = DataLoader(valid_data, batch_size=batch_size, collate_fn=collate_fn)
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -38,18 +40,23 @@ def main():
 
     for epoch in range(1, epoch_num + 1):
         start_time = time.time()
-        losses = []
-        for i_batch, batch_data in enumerate(dataloader):
+        train_losses = []
+        for i_batch, batch_data in enumerate(train_dataloader):
             optimizer.zero_grad()
             loss = model(batch_data)
             loss.backward()
             optimizer.step()
-            losses.append(loss.tolist())
+            train_losses.append(loss.tolist())
 
             if (i_batch + 1) % 10 == 0:
-                print("Epoch %d Batch %d, train loss %f" % (epoch, i_batch, np.mean(losses[-10:])))
+                print("Epoch %d Batch %d, train loss %f" % (epoch, i_batch, np.mean(train_losses[-10:])), flush=True)
 
-        print("Epoch " + str(epoch) + " finished, took " + str(time.time() - start_time) + "s")
+        valid_losses = []
+        for i_batch, batch_data in enumerate(valid_dataloader):
+            loss = model(batch_data)
+            valid_losses.append(loss.tolist())
+        print("Epoch " + str(epoch) + " finished, took " + str(time.time() - start_time) + "s", flush=True)
+        print("valid loss " + str(np.mean(valid_losses)), flush=True)
 
         with open(os.path.join(model_save_path, 'model_%s' % name), "wb") as f:
             torch.save(model, f)
