b20b83d1dd0f8befe3fc7aa96ffdcdc29244dd56
Huang <498169842@qq.com>
2021-12-17 23:31:17 +0800

    add [UNK]

--- gen_vocab.py
+++ gen_vocab.py
@@ -1,4 +1,5 @@
 from gensim.models.word2vec import Word2Vec
+import numpy as np
 
 def convert(list_of_floats):
     return " ".join([("%.6f" % f) for f in list_of_floats])
@@ -11,6 +12,9 @@ for line in open("data/cut_valid.txt", "r", encoding="utf-8"):
 
 w2v = Word2Vec(lines, vector_size=300, min_count=1)
 
+vec = np.mean([w2v.wv[key] for key in w2v.wv.key_to_index.keys()], axis=1)
+
 with open("vocab/vocab.txt", "w", encoding="utf8") as f:
+    f.write("[UNK]" + " " + convert(vec) + "\n")
     for key in w2v.wv.key_to_index.keys():
         f.write(key + " " + convert(w2v.wv[key]) + "\n")
